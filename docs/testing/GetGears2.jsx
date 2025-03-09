a2c139ebf0aa3ef14fc93518b627494b17d6f817
Phil Kaighin <pkaig@westnet.com.au>
2025-03-09 17:25:35 +1000

    Hurricane

--- docs/testing/GetGears2.jsx
+++ docs/testing/GetGears2.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import Gears from './gears.json';
+import Gears2 from './gears2.json';
 import 'bootstrap/dist/css/bootstrap.css';
 import styles2 from './gears2.module.css'
 import gearPic2 from './img/gears2.png'
 
-const heliGear = Gears.heliGears
+const heliGear = Gears2.heliGears
 let selectIndex = 0;
 
 function GetGears() {
@@ -31,12 +31,12 @@ const handleOnChange = (e)=>{
 }
 
 const handleGearChange = ()=> {
-    setZ1(document.getElementById('-z1').value);
-    setZ2(document.getElementById('-z2').value);
-    setZ3(document.getElementById('-z3').value);
-    setZ4(document.getElementById('-z4').value);
-    setZ5(document.getElementById('-z5').value);
-    setZ6(document.getElementById('-z6').value);
+    setZ1(document.getElementById('z1_2').value);
+    setZ2(document.getElementById('z2_2').value);
+    setZ3(document.getElementById('z3_2').value);
+    setZ4(document.getElementById('z4_2').value);
+    setZ5(document.getElementById('z5_2').value);
+    setZ6(document.getElementById('z6_2').value);
 }
 
     return (
@@ -58,27 +58,27 @@ const handleGearChange = ()=> {
                 </div>
             </div>
             <div>
-                <input className={styles2.z1} type='number' id="-z1" onChange={ handleGearChange } value={z1}></input>
-                <input className={styles2.z2} type='number' id="-z2" onChange={ handleGearChange } value={z2}></input>
-                <input className={styles2.z3} type='number' id="-z3" onChange={ handleGearChange } value={z3}></input>
-                <input className={styles2.z4} type='number' id="-z4" onChange={ handleGearChange } value={z4}></input>
-                <input className={styles2.z5} type='number' id="-z5" onChange={ handleGearChange } value={z5}></input>
-                <input className={styles2.z6} type='number' id="-z6" onChange={ handleGearChange } value={z6}></input>
+                <input className={styles2.z1} type='number' id="z1_2" onChange={ handleGearChange } value={z1}></input>
+                <input className={styles2.z2} type='number' id="z2_2" onChange={ handleGearChange } value={z2}></input>
+                <input className={styles2.z3} type='number' id="z3_2" onChange={ handleGearChange } value={z3}></input>
+                <input className={styles2.z4} type='number' id="z4_2" onChange={ handleGearChange } value={z4}></input>
+                <input className={styles2.z5} type='number' id="z5_2" onChange={ handleGearChange } value={z5}></input>
+                <input className={styles2.z6} type='number' id="z6_2" onChange={ handleGearChange } value={z6}></input>
             </div>
             <div className={styles2.outContainer}>
                 <div className={styles2.out}>
                     <span>
-                        <input key='main1' disabled='true' type='number' value={z1*z3}></input>
+                        <input key='main1_2' disabled='true' type='number' value={z1*z3}></input>
                         <b> : </b>
-                        <input key='main2' disabled='true' type='number' value={z2*z4}></input>
+                        <input key='main2_2' disabled='true' type='number' value={z2*z4}></input>
                         <b>Calculated Main Ratio</b>
                     </span>
                 </div>
                 <div className={styles2.out}>
                     <span >
-                        <input key='tail1' disabled='true' type='number' value={z6}></input>
+                        <input key='tail1_2' disabled='true' type='number' value={z6}></input>
                         <b> : </b>
-                        <input key='tail2' disabled='true' type='number' value={z5}></input>
+                        <input key='tail2_2' disabled='true' type='number' value={z5}></input>
                         <b>Calculated Tail Ratio</b>
                     </span>
                 </div>
---
+++ docs/testing/GetGears3.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import Gears3 from './gears3.json';
+import 'bootstrap/dist/css/bootstrap.css';
+import styles3 from './gears3.module.css'
+import gearPic3 from './img/gears3.png'
+
+const heliGear = Gears3.heliGears
+let selectIndex = 0;
+
+function GetGears() {
+
+const [ heli, setHeli ] = useState('Heli-Default');
+const [ z1, setZ1 ] = useState('15');
+const [ z2, setZ2 ] = useState('60');
+const [ z3, setZ3 ] = useState('19');
+const [ z4, setZ4 ] = useState('61');
+const [ z5, setZ5 ] = useState('14');
+const [ z6, setZ6 ] = useState('9');
+const [ z7, setZ7 ] = useState('9');
+
+const handleOnChange = (e)=>{
+    setHeli(e.target.value);
+    selectIndex = document.getElementById('select3').selectedIndex  ; 
+    console.log(document.getElementById('select3'))
+
+    setZ1(heliGear[selectIndex].z1);
+    setZ2(heliGear[selectIndex].z2);
+    setZ3(heliGear[selectIndex].z3);
+    setZ4(heliGear[selectIndex].z4);
+    setZ5(heliGear[selectIndex].z5);
+    setZ6(heliGear[selectIndex].z6);
+    setZ7(heliGear[selectIndex].z7);
+}
+
+const handleGearChange = ()=> {
+    setZ1(document.getElementById('z1_3').value);
+    setZ2(document.getElementById('z2_3').value);
+    setZ3(document.getElementById('z3_3').value);
+    setZ4(document.getElementById('z4_3').value);
+    setZ5(document.getElementById('z5_3').value);
+    setZ6(document.getElementById('z6_3').value);
+    setZ7(document.getElementById('z7_3').value);
+}
+
+    return (
+<>
+    <form className={styles3.wrapper} position='relative'>
+        <div className={styles3.image}>
+            <img src={gearPic3} class="img-thumbnail" alt="gears"></img>
+        
+            <div className={styles3.content}>
+                <div>
+                    <p>Type 2: two stage gear</p>
+                </div>
+                <div className="mt-4">
+                    <select className={styles3.drop} id="select3" onChange={ handleOnChange } value={heli}>
+                    {heliGear.map((g)=>
+                        (g.type===3 && <option id={g.index} value={g.name}>{g.name}</option>)
+                    )}
+                    </select>
+                </div>
+            </div>
+            <div>
+                <input className={styles3.z1} type='number' id="z1_3" onChange={ handleGearChange } value={z1}></input>
+                <input className={styles3.z2} type='number' id="z2_3" onChange={ handleGearChange } value={z2}></input>
+                <input className={styles3.z3} type='number' id="z3_3" onChange={ handleGearChange } value={z3}></input>
+                <input className={styles3.z4} type='number' id="z4_3" onChange={ handleGearChange } value={z4}></input>
+                <input className={styles3.z5} type='number' id="z5_3" onChange={ handleGearChange } value={z5}></input>
+                <input className={styles3.z6} type='number' id="z6_3" onChange={ handleGearChange } value={z6}></input>
+                <input className={styles3.z7} type='number' id="z7_3" onChange={ handleGearChange } value={z7}></input>
+            </div>
+            <div className={styles3.outContainer}>
+                <div className={styles3.out}>
+                    <span>
+                        <input key='main1-3' disabled='true' type='number' value={z1*z3}></input>
+                        <b> : </b>
+                        <input key='main2-3' disabled='true' type='number' value={z2*z4}></input>
+                        <b>Calculated Main Ratio</b>
+                    </span>
+                </div>
+                <div className={styles3.out}>
+                    <span >
+                        <input key='tail1-3' disabled='true' type='number' value={z5*z7}></input>
+                        <b> : </b>
+                        <input key='tail2-3' disabled='true' type='number' value={z4*z6}></input>
+                        <b>Calculated Tail Ratio</b>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </form>
+
+</>
+    )
+}
+
+export default GetGears
