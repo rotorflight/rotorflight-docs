81dd3e09baa98628225af32f7e7b4f0214d47941
Vasil Nikolaev Nikolov <vasil.nikolov@commasense.tech>
2026-03-12 15:41:45 +0200

    Adjust lint scripts

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,11 @@
 export default {
-  "*.{js,jsx,ts,tsx,md,mdx,json,yml,yaml,css,scss,html}": [
-    "npm run prettier:check --",
-    "npm run spellcheck --",
+  "*.{js,jsx,ts,tsx,json,yml,yaml,css,scss,html}": [
+    "npm run prettier:check:staged --",
+    "npm run spellcheck:staged --",
   ],
-  "*.{md,mdx}": ["remark --output --"],
+  "*.{md,mdx}": [
+    "remark --output --",
+    "npm run prettier:check:staged --",
+    "npm run spellcheck:staged --",
+  ],
 };
